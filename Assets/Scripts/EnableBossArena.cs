56ed8132577f0d41731306794846abe21459c38a
TelorAmmar <hafidzammar3@gmail.com>
2024-05-31 22:09:55 +0700

    Enable boss saat memasuki arena

--- Assets/Scripts/EnableBossArena.cs
+++ Assets/Scripts/EnableBossArena.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class EnableBossArena : MonoBehaviour
 {
     public GameObject Arena;
+    public GameObject Boss;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (Arena != null && !Arena.activeInHierarchy && tag == "EnableArena")
         {
             Arena.SetActive(true);
+            Boss.SetActive(true);
+            Destroy(gameObject);
         }
         else if(tag == "DisableArena")
         {
