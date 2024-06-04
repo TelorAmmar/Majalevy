17d852db480697756fd0d0b5c0a820d2572595d9
TelorAmmar <hafidzammar3@gmail.com>
2024-06-05 05:33:33 +0700

    Ganti script MoveStage agar hanya bisa ditrigger oleh player

--- Assets/Scripts/MoveStage.cs
+++ Assets/Scripts/MoveStage.cs
@@ -9,11 +9,12 @@ public class MoveStage : MonoBehaviour
     public GameObject panel;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.gameObject.tag == "Player")
+        {
             if (panel != null)
             {
                 panel.SetActive(true);
             };
-        
+        }
     }
 }
